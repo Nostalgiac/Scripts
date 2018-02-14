00f4e1e9dc44d4369ad27043435aef02d6e5490d
Nostalgiac <Nostalgiac@users.noreply.github.com>
2018-02-14 18:03:01 +0800

    Update TorrentLog.sh
    Added time formatting.

--- TorrentLog.sh
+++ TorrentLog.sh
@@ -9,17 +9,19 @@ for TORRENTID in $TORRENTLIST
 do
         TIME=`transmission-remote localhost:9091 --torrent $TORRENTID --info | grep "Seeding Time" | awk -F '[()]' '{print $2}' | sed 's/ seconds//g'`
         NAME=`transmission-remote localhost:9091 --torrent $TORRENTID --info | grep "Name"`
+        FTIME=`printf '%dd:%dh:%dm:%ds\n' $((${TIME}/86400)) $((${TIME}%86400/3600)) $((${TIME}%3600/60)) $((${TIME}%60))`
+
 
         if [ "$TIME" -gt "1814400" ]
         then
-                echo "Stopping Torrent ${NAME} after ${TIME} seconds" >> torrentlog.log
+                echo "Stopping Torrent ${NAME} after ${FTIME}" >> torrentlog.log
                 transmission-remote localhost:9091 -t $TORRENTID --stop &
         elif [ -z "$TIME" ]
         then
                 PCT=`transmission-remote localhost:9091 --torrent $TORRENTID --info | grep "Percent Done"`
                 echo "${NAME} is ${PCT}" >> torrentlog.log
         else
-                echo "${NAME} has been running for ${TIME} seconds" >> torrentlog.log
+                echo "${NAME} has been running for ${FTIME}" >> torrentlog.log
         fi
 
 done
