a7a6e827f82c8134d2c6541144e88d74e0cdea0a
Nostalgiac <Nostalgiac@users.noreply.github.com>
2019-02-22 12:51:06 +0800

    Update SCCM Snippets.ps1

--- SCCM Snippets/SCCM Snippets.ps1
+++ SCCM Snippets/SCCM Snippets.ps1
@@ -4,7 +4,20 @@
 $collections = Get-WmiObject -Namespace "root\SMS\Site_CIT" -Class SMS_Collection | where {($_.LimitToCollectionID -eq "SMS00001") -and ($_.ObjectPath -like "/Subfolder Name")}
 
 foreach($coll in $collections){
- $coll.LimitToCollectionName = "New Limiting Collection Name)"
+ $coll.LimitToCollectionName = "New Limiting Collection Name"
  $coll.LimitToCollectionID = "New Limiting Collection ID"
  $coll.Put()
 }
+
+
+
+#Add User Group Resource (AD Security Group) as a Direct Membership Rule
+$SiteCode = "XXX"
+$collectionName = "User Collection Name"
+$ADGroupName = "Domain\SecGrp"
+$SCCMServer = "primary-server.domain.com"
+
+$UserGroups = Get-WmiObject -Namespace "root\sms\site_$($SiteCode)" -Class 'SMS_R_UserGroup' -ComputerName $SCCMServer
+$GroupToAdd = $UserGroups | Where Name -eq $ADGroupName
+
+ Add-CMUserCollectionDirectMembershipRule -CollectionName $collectionName -ResourceId $GroupToAdd.ResourceId
